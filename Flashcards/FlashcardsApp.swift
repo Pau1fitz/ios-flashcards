3e45c896984caf78d29ff30b572c65e9a8b6136b
Paul Fitzgerald <paulfitz99@gmail.com>
2023-04-07 19:59:37 +0100

    Initial Commit

---
+++ Flashcards/FlashcardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashcardsApp.swift
+//  Flashcards
+//
+//  Created by Paul Fitzgerald on 07/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct FlashcardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
